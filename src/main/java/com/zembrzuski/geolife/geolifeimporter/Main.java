774059eff3922a395007192e5d364ff42d4a4925
Your Name <you@example.com>
2018-01-16 20:28:20 -0200

    main

--- src/main/java/com/zembrzuski/geolife/geolifeimporter/Main.java
+++ src/main/java/com/zembrzuski/geolife/geolifeimporter/Main.java
@@ -5,17 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.List;
-import java.util.TreeMap;
-
 @SpringBootApplication
-public class GeolifeImporterApplication implements CommandLineRunner {
+public class Main implements CommandLineRunner {
 
     @Autowired
     private DirectoryProcessor directoryProcessor;
 
     public static void main(String[] args) {
-        SpringApplication.run(GeolifeImporterApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
     @Override
--- src/test/java/com/zembrzuski/geolife/geolifeimporter/MainTests.java
+++ src/test/java/com/zembrzuski/geolife/geolifeimporter/MainTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GeolifeImporterApplicationTests {
+public class MainTests {
 
 	@Test
 	public void contextLoads() {
